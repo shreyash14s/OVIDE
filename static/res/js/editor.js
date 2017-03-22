39d5aebfdf8a8c09df9bbf6d59e063c34d5d8bdb
s-s-rao <ssrao96@gmail.com>
2017-03-22 12:41:28 +0530

    non-significant changes

--- static/res/js/editor.js
+++ static/res/js/editor.js
@@ -66,29 +66,30 @@ $(function() {
 		$("#langbtn").val($(this).text()  + "  <span class=\"caret\"></span>");
 	})
 	
-	$("#fileUpload").change(
-				function()
-				{
-					input = document.getElementById('fileUpload');
-					if (!input) {
-     				 alert("Um, couldn't find the fileinput element.");
-    				}
-					 else if (!input.files) {
-     				 alert("This browser doesn't seem to support the `files` property of file inputs.");
-    			}
-    			else if (!input.files[0]) {
-      				alert("Please select a file before clicking 'Load'");               
-    			}
-    			  else 
-    			  {
-				      file = input.files[0];
-				      fr = new FileReader();
-				      fr.onload = receivedText;
-				      //fr.readAsText(file);
-				      fr.readAsText(file);
-	   			 }
-				}
-		)
+	$("#fileUpload").change(function()
+	{
+		input = document.getElementById('fileUpload');
+		if (!input)
+		{
+			 alert("Um, couldn't find the fileinput element.");
+		}
+		else if (!input.files)
+		{
+			 alert("This browser doesn't seem to support the `files` property of file inputs.");
+		}
+		else if (!input.files[0])
+		{
+				alert("Please select a file before clicking 'Load'");               
+		}
+		else 
+		{
+			file = input.files[0];
+			fr = new FileReader();
+			fr.onload = receivedText;
+			//fr.readAsText(file);
+			fr.readAsText(file);
+		}
+	});
 
 	
 });
