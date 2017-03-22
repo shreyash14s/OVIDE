18100abb7f39d3bec319e79b88f3280e3b2629b4
s-s-rao <ssrao96@gmail.com>
2017-03-22 11:16:58 +0530

    First step to success
    The title says it all

---
+++ ex.py
@@ -0,0 +1,55 @@
+from flask import Flask, redirect, url_for, request, render_template, Response
+import os
+import subprocess as sb
+import sys
+import json
+
+
+def toJson(otput,error,s):
+    dick = {}
+    dick['out'] = otput
+    dick['error'] = error
+    dick['status'] = s
+    return json.dumps(dick)
+
+app = Flask(__name__)
+app.config["CACHE_TYPE"] = "null"
+
+@app.route('/')
+def serve():
+    return render_template('editor.html')
+
+@app.route('/compile',methods = ['POST', 'GET'])
+def compile():
+    #if request.method == 'POST':    
+    lang = request.form['lang']
+    if lang == 'C':
+        f = open('temp.c','w')
+        f.write(request.form['code'])
+        f.close()
+        ret=sb.Popen("gcc -o temp temp.c",shell=True,stdout=sb.PIPE,stderr=sb.PIPE,close_fds=True)
+    # elif ext == 'java':
+    #     ret=sb.Popen("javac " + file,shell=True,stdout=sb.PIPE,stderr=sb.PIPE,close_fds=True)
+    else:
+        return toJson("","Compile","Cannot Compile!!")
+    out,err=ret.stdout.read(),ret.stderr.read()
+    resp = Response(toJson(out,err,"Compiled!!"))
+    resp.headers['Access-Control-Allow-Origin'] = '*'
+    return resp
+    
+@app.route('/run',methods = ['POST', 'GET'])
+def run():
+    lang = request.form['lang']
+    out=""
+    err=""
+    stat="Can't Run"
+    if lang == "C":
+        ret = sb.Popen("./temp" ,shell=True,stdout=sb.PIPE,stderr=sb.PIPE,close_fds=True)
+        out,err=ret.stdout.read(),ret.stderr.read()
+        stat="Running..."
+    resp = Response(toJson(out,err,"Running!!"))
+    resp.headers['Access-Control-Allow-Origin'] = '*'
+    return resp
+
+if __name__== '__main__':
+    app.run(debug=True)
---
+++ static/res/js/editor.js
@@ -0,0 +1,134 @@
+// All this code just to override the dafault event listener for pressing 'tab' button on textarea
+
+(function($) {
+	function pasteIntoInput(el, text) {
+		el.focus();
+		if (typeof el.selectionStart == "number") {
+			var val = el.value;
+			var selStart = el.selectionStart;
+			el.value = val.slice(0, selStart) + text + val.slice(el.selectionEnd);
+			el.selectionEnd = el.selectionStart = selStart + text.length;
+		} else if (typeof document.selection != "undefined") {
+			var textRange = document.selection.createRange();
+			textRange.text = text;
+			textRange.collapse(false);
+			textRange.select();
+		}
+	}
+
+	function allowTabChar(el) {
+		$(el).keydown(function(e) {
+			if (e.which == 9) {
+				pasteIntoInput(this, "\t");
+				return false;
+			}
+		});
+
+		$(el).keypress(function(e) {
+			if (e.which == 9) {
+				return false;
+			}
+		});
+	}
+
+	$.fn.allowTabChar = function() {
+		if (this.jquery) {
+			this.each(function() {
+				if (this.nodeType == 1) {
+					var nodeName = this.nodeName.toLowerCase();
+					if (nodeName == "textarea" || (nodeName == "input" && this.type == "text")) {
+						allowTabChar(this);
+					}
+				}
+			})
+		}
+		return this;
+	}
+})(jQuery);
+
+function receivedText() {
+    document.getElementById('text').value = document.createTextNode(fr.result).nodeValue;
+  } 
+
+$(function() {
+	$("textarea").allowTabChar();
+	jedit = $("#editor");
+	joutput = $("#output");
+	jtext = $("#text");
+	var jcomp = $("#compile");
+	var jrun = $("#run");
+	jcomp.bind("click", compileIt);
+	jrun.bind("click", runIt);
+
+
+	$("#langsch li a").click(function () {
+		$("#langbtn").html($(this).text() + "  <span class=\"caret\"></span>");
+		$("#langbtn").val($(this).text()  + "  <span class=\"caret\"></span>");
+	})
+	
+	$("#fileUpload").change(
+				function()
+				{
+					input = document.getElementById('fileUpload');
+					if (!input) {
+     				 alert("Um, couldn't find the fileinput element.");
+    				}
+					 else if (!input.files) {
+     				 alert("This browser doesn't seem to support the `files` property of file inputs.");
+    			}
+    			else if (!input.files[0]) {
+      				alert("Please select a file before clicking 'Load'");               
+    			}
+    			  else 
+    			  {
+				      file = input.files[0];
+				      fr = new FileReader();
+				      fr.onload = receivedText;
+				      //fr.readAsText(file);
+				      fr.readAsDataURL(file);
+	   			 }
+				}
+		)
+
+	
+});
+
+function compileIt() {
+	$.ajax({
+		url: "http://localhost:5000/compile",
+		type: "POST",
+		data: {
+			code: jtext.val(),
+			lang: "C",
+		},
+		dataType: "json", // Confirm it.
+		success: compileComplete,
+		error: failedToSend
+	});
+}
+
+function compileComplete(data) {
+	//alert(	)
+	alert(JSON.stringify(data))
+}
+
+function failedToSend(data) {
+	alert("Failed to make AJAX call");
+}
+
+function runIt() {
+	$.ajax({
+		url: "http://localhost:5000/run",
+		type: "POST",
+		data: {
+			lang: "C"
+		},
+		dataType: "json", // Confirm it.
+		success: runComplete,
+		error: failedToSend
+	});
+}
+
+function runComplete(data) {
+	alert(JSON.stringify(data))
+}
